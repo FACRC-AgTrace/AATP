43794e9ee9c56192b1adf284bc64d3826339b687
Ashley Harwood <60303491+ashleythedeveloper@users.noreply.github.com>
2024-12-03 16:53:27 +1100

    chore: update website config (#2)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -12,14 +12,14 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://FACRC-AgTrace.github.io',
+  url: 'http://aatp.foodagility.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/AATP/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'AgTrace', // Usually your GitHub org/user name.
+  organizationName: 'FACRC-AgTrace', // Usually your GitHub org/user name.
   projectName: 'AATP', // Usually your repo name.
 
   onBrokenLinks: 'throw',
